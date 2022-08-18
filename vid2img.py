0624ae067beda6c218ea434705f0c905eaaad523
Bo Shang <cnpcshangbo@gmail.com>
2022-08-18 16:01:14 -0500

    vo works for 114, image too large

---
+++ vid2img.py
@@ -0,0 +1,44 @@
+import cv2
+
+cap = cv2.VideoCapture('/usr/local/home/bsr8w/Downloads/P0440046.MP4')
+
+if (cap.isOpened() == False): 
+  print("Unable to read camera feed")
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+count = 0
+while(True):
+  ret, frame = cap.read()
+  scale_percent = 50 # percent of original size
+  width = int(img.shape[1] * scale_percent / 100)
+  height = int(img.shape[0] * scale_percent / 100)
+  dim = (width, height)
+  
+  # resize image
+  resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+  k = cv2.waitKey(1)
+
+  if ret == True: 
+    cv2.imshow('frame',resized)
+
+    # press space key to start recording
+    if True: #k%256 == 32:
+        cv2.imwrite("rgb_%05d.jpg" % count, resized) 
+        count += 1
+        #cv2.imwrite()
+        #out.write(frame) 
+
+    # press q key to close the program
+    elif k & 0xFF == ord('q'):
+        break
+
+  else:
+     break  
+
+cap.release()
+out.release()
+
+cv2.destroyAllWindows() 
